22a683ec5ed9a938b708a8bde9b1c5efa6bfba27
Sertavr <pampuh.net@gmail.com>
2025-10-30 10:11:41 +0200

    make build

---
+++ js_task_fix_form_DOM.f75de5e1.js
@@ -0,0 +1,31 @@
+'use strict';
+const collectionInputs = document.querySelectorAll('form input');
+const inputs = Array.from(collectionInputs);
+const collectionDivField = document.querySelectorAll('.field');
+function modifiesString(str) {
+    let result = '';
+    for(let i = 0; i < str.length; i++){
+        if (i === 0) {
+            result += str[i].toUpperCase();
+            continue;
+        }
+        if (str[i] === str[i].toUpperCase()) {
+            result += ` ${str[i].toLowerCase()}`;
+            continue;
+        }
+        result += str[i];
+    }
+    return result;
+}
+collectionDivField.forEach((fied, ind)=>{
+    const labelEl = document.createElement('label');
+    const idInput = inputs[ind].getAttribute('id');
+    const nameImput = inputs[ind].getAttribute('name');
+    inputs[ind].setAttribute('placeholder', modifiesString(nameImput));
+    labelEl.setAttribute('for', idInput);
+    labelEl.classList.add('field-label');
+    labelEl.textContent = modifiesString(nameImput);
+    fied.insertAdjacentElement('afterbegin', labelEl);
+});
+
+//# sourceMappingURL=js_task_fix_form_DOM.f75de5e1.js.map
