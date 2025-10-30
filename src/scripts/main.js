56809bc3bf7ba2fe49592fe31d5b3ee1e11e941f
Sertavr <pampuh.net@gmail.com>
2025-10-30 09:07:46 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const collectionInputs = document.querySelectorAll('input');
+const inputs = Array.from(collectionInputs);
+const collectionDivField = document.querySelectorAll('.field');
+
+collectionDivField.forEach((fied, ind) => {
+  const labelEl = document.createElement('label');
+  const idInput = inputs[ind].getAttribute('id');
+  const nameImput = inputs[ind].getAttribute('name');
+
+  inputs[ind].setAttribute('placeholder', `${nameImput.toUpperCase()}`);
+
+  labelEl.setAttribute('for', idInput);
+  labelEl.classList.add('field-label');
+  labelEl.textContent = nameImput;
+
+  fied.insertAdjacentElement('beforeend', labelEl);
+});
