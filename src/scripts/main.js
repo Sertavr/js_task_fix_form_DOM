82674e8f81ac6e49650815386aac3ea545a6b6ad
Sertavr <pampuh.net@gmail.com>
2025-10-30 09:54:25 +0200

    chenged format placeholder text and label element position

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,16 +4,36 @@ const collectionInputs = document.querySelectorAll('input');
 const inputs = Array.from(collectionInputs);
 const collectionDivField = document.querySelectorAll('.field');
 
+function modifiesString(str) {
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (i === 0) {
+      result += str[i].toUpperCase();
+      continue;
+    }
+
+    if (str[i] === str[i].toUpperCase()) {
+      result += ` ${str[i].toLowerCase()}`;
+      continue;
+    }
+
+    result += str[i];
+  }
+
+  return result;
+}
+
 collectionDivField.forEach((fied, ind) => {
   const labelEl = document.createElement('label');
   const idInput = inputs[ind].getAttribute('id');
   const nameImput = inputs[ind].getAttribute('name');
 
-  inputs[ind].setAttribute('placeholder', `${nameImput.toUpperCase()}`);
+  inputs[ind].setAttribute('placeholder', modifiesString(nameImput));
 
   labelEl.setAttribute('for', idInput);
   labelEl.classList.add('field-label');
   labelEl.textContent = nameImput;
 
-  fied.insertAdjacentElement('beforeend', labelEl);
+  fied.insertAdjacentElement('afterbegin', labelEl);
 });
